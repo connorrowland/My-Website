b6db8ee054081d7c8630a09135e3cca9f658759f
Connor Rowland <cjr6734@yahoo.com>
2015-09-01 11:52:00 -0400

    changed menu bar

--- website.js
+++ website.js
@@ -2,19 +2,19 @@ $(window).load(function(){
 	//Menu bar expansion on click
 	
 	var hamburgerIcon = $("#hamburgerIcon");
-	var xOutMenu = $("p");
+	var xOutMenu = $("#xOut");
 
 	//adds a class of hidden which to hamburgerIcon sets the hamburger icon's dispaly to none, making it invisible
 	  hamburgerIcon.click(function (){
-	    $("nav").toggle("slow");
-	    $(xOutMenu).removeClass("hidden");
-	    $(this).addClass("hidden");
+	    $("nav").toggle();
+	    // $(this).addClass("hidden");
+	    // $(xOutMenu).removeClass("hidden");
 	  });
 
 	  //adds a class of hidden to the X out P tag which sets it's dispaly to none, making it invisible.
-	  $("p").click(function () {
-	    $("nav").toggle("slow");
-	    hamburgerIcon.removeClass("hidden");
-	    $(this).addClass("hidden");
+	  $(xOutMenu).click(function () {
+	    $("nav").toggle();
+	    // hamburgerIcon.removeClass("hidden");
+	    // $("nav").addClass("hidden");
 	  });
 });
