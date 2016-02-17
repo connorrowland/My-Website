31601aef5038373324e1ec0ec24f04a80004a03f
Connor Rowland <cjr6734@yahoo.com>
2016-02-16 20:13:42 -0500

    addition of iphone page

--- app.js
+++ app.js
@@ -4,4 +4,13 @@ $(document).ready(function() {
 	$(window).scroll(function(){
 	    $(".lander_container").css("opacity", 1 - $(window).scrollTop() / 500);
 	  });
+
+	////////interactive iphone/////////
+
+	//turn on and off iphone
+	$(".home_button").click(function(){
+		$(".off_iphone_screen").css("display","none");
+		$(".on_iphone_screen").fadeIn(1000);
+		$(".on_iphone_screen").css("display","inline-block");
+	})
 });
