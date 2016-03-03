16dc600245857187da978d8efab35c105e28a661
Connor Rowland <cjr6734@yahoo.com>
2016-03-02 20:42:15 -0500

    adding footer stuff and iphone stuff

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ $(document).ready(function() {
 	    $(".lander_container").css("opacity", 1 - $(window).scrollTop() / 500);
 	  });
 
+	//scroll to contact in footer on click of contact button
+	$("#contact_link").click(function(){
+    	$('html, body').animate({
+        	scrollTop: $("#footer_contact_box").offset().top
+    	}, 800);
+	});
+
 	////////interactive iphone/////////
 
 	//turn on and off iphone
@@ -12,5 +19,12 @@ $(document).ready(function() {
 		$(".off_iphone_screen").css("display","none");
 		$(".on_iphone_screen").fadeIn(1000);
 		$(".on_iphone_screen").css("display","inline-block");
-	})
+	});
+	//switch screens to personal site iframe
+	$("#personal_site_app").click(function(){
+		$(".on_iphone_screens").css("display","none");
+		$("#personal_site_frame").fadeIn(1000);
+		$("#personal_site_frame").css("display","inline-block");
+	});
+
 });
